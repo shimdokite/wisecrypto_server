d771546783ceea7750fed716eec0625f2279d88f
심도연 <shimdokite@gmail.com>
2024-01-19 17:16:23 +0900

    ✨ Button 컴포넌트 구현

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,17 @@
+interface ButtonProps {
+  children: string;
+  color: 'green' | 'red';
+}
+
+export default function Button({ children, color }: ButtonProps) {
+  return (
+    <button
+      className={`${
+        color === 'green'
+          ? 'bg-Primary-1 border-Primary-1'
+          : 'bg-Danger-1 border-Danger-1'
+      } w-full py-[11px] rounded-[4px] text-White-1 text-xs font-semiBold`}>
+      {children}
+    </button>
+  );
+}
