6bda63ec178d2d6b18ad6874694ee6ef3ddb717f
심도연 <shimdokite@gmail.com>
2024-01-19 17:15:13 +0900

    💄 token 추가

---
+++ client/src/styles/tokens.js
@@ -0,0 +1,43 @@
+const tokens = {
+  colors: {
+    "Primary": {
+      "1": "#5EDE99",
+    },
+    "Secondary": {
+      "1": "#F288F4",
+    },
+    "Success": {
+      "1": "#00CB6A",
+    },
+    "Warning": {
+      "1": "#EFBE24",
+    },
+    "Danger": {
+      "1": "#F26666",
+    },
+    "Dark": {
+      "1": "#04150C",
+    },
+    "Light": {
+      "1": "#F7F7F7",
+    },
+    "Black": {
+      "1": "#152C07",
+    },
+    "Gray": {
+      "1": "#5D5C5D"
+    },
+    "Gray2": {
+      "2": "#BEBEBE",
+    },
+    "White": {
+      "1": "#FFFFFF",
+    },
+  },
+  
+  boxShadow: {
+    "inputShadow": "0px 2px 10px 0px rgba(51, 51, 51, 10%)"
+  }
+}
+
+export default tokens;
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from 'tailwindcss';
 
+import tokens from './src/styles/tokens';
+
+const { colors, boxShadow } = tokens;
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +11,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors,
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      boxShadow,
       fontFamily: {
         montserrat: ['var(--montserrat)'],
       },
