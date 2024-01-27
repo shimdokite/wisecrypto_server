a37e4491b239564ff23bb78cbb0db45eb752dbff
shimdokite <shimdokite@gmail.com>
2024-01-27 23:13:03 +0900

    ✨ Login 페이지 submit 기능 구현

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,11 +1,31 @@
 import SignInput from './SignInput';
 
-export default function LoginForm() {
+import { signForm } from 'types/data';
+
+interface LoginFormProps {
+  values: signForm;
+
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function LoginForm({ values, handleChange }: LoginFormProps) {
   return (
     <form className="flex flex-col gap-4">
-      <SignInput children="Email" placeholder="botpablo@gmail.com" />
+      <SignInput
+        children="Email"
+        placeholder="botpablo@gmail.com"
+        handleChange={handleChange}
+        value={values}
+        tag="email"
+      />
 
-      <SignInput children="Kata Sandi" placeholder="Masukkan Kata Sandi" />
+      <SignInput
+        children="Kata Sandi"
+        placeholder="Masukkan Kata Sandi"
+        handleChange={handleChange}
+        value={values}
+        tag="password"
+      />
     </form>
   );
 }
