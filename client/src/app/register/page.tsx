890156d951a76737e5cbc12be1fd769be6897de4
shimdokite <shimdokite@gmail.com>
2024-01-27 23:11:49 +0900

    ✨ Register 페이지 submit 기능 구현

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,8 +1,44 @@
+'use client';
+
 import Image from 'next/image';
 
-import { Button, Logo, RegisterForm, SignBottom, SignTop } from 'components';
+import {
+  Button,
+  Logo,
+  RegisterCheck,
+  RegisterForm,
+  SignBottom,
+  SignTop,
+} from 'components';
+
+import useForm from 'hooks/useForm';
+
+import { signForm } from 'types/data';
 
 export default function Register() {
+  const { next, setNext, values, handleChange, handleSubmit } = useForm({
+    initialValue: {
+      name: '',
+      email: '',
+      password: '',
+      phone: '',
+      role: '',
+      passwordCheck: '',
+      check: 'off',
+    },
+    onSubmit: (values: signForm) => {
+      if (next === true && values.check === 'on') {
+        alert(JSON.stringify(values, null, 2));
+      }
+    },
+  });
+
+  const onNext = () => {
+    if (values.name === '' || values.phone === '') return;
+
+    setNext(true);
+  };
+
   return (
     <section className="w-full h-full">
       <div className="mx-[15px]">
@@ -13,10 +49,31 @@ export default function Register() {
             <SignTop role="register" />
 
             <div className="flex flex-col gap-6">
-              <RegisterForm />
+              <RegisterForm
+                next={next}
+                values={values}
+                handleSubmit={handleSubmit}
+                handleChange={handleChange}
+              />
 
               <div className="flex flex-col gap-6 justify-center items-center">
-                <Button children="SELANJUTNYA" color="green" />
+                {next ? (
+                  <>
+                    <RegisterCheck handleChange={handleChange} />
+
+                    <Button
+                      children="DAFTAR"
+                      color="green"
+                      onClick={handleSubmit}
+                    />
+                  </>
+                ) : (
+                  <Button
+                    children="SELANJUTNYA"
+                    color="green"
+                    onClick={onNext}
+                  />
+                )}
 
                 <SignBottom role="register" />
               </div>
@@ -24,20 +81,24 @@ export default function Register() {
           </div>
         </div>
 
-        <div className="flex justify-end pt-12">
-          <button className="flex items-center text-Gray2-1">
-            <p className="py-2 pr-[2px] text-[11px] underline">
-              Lewati langkah ini
-            </p>
+        {!next && (
+          <div className="flex justify-end pt-12">
+            <button
+              className="flex items-center text-Gray2-1"
+              onClick={() => setNext(!next)}>
+              <p className="py-2 pr-1 text-[11px] underline">
+                Lewati langkah ini
+              </p>
 
-            <Image
-              src="/assets/icon/right_arrow.svg"
-              alt="right arrow"
-              width={10.67}
-              height={10.37}
-            />
-          </button>
-        </div>
+              <Image
+                src="/assets/icon/right_arrow.svg"
+                alt="right arrow"
+                width={10.67}
+                height={10.37}
+              />
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
