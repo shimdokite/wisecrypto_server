e964c18c0bcd00e14711ff776a5b07aa88f8450a
shimdokite <shimdokite@gmail.com>
2024-02-17 01:49:01 +0900

    ✨ Home 컴포넌트 UI 구현 (헤더와 푸터(메뉴바) 제외)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,33 +1,44 @@
 import Assets from 'components/Assets';
+import Balance from 'components/Balance';
 import ProfileFrame from 'components/ProfileFrame';
+import TotalPortfolio from 'components/TotalPortfolio';
 import Watchlist from 'components/Watchlist';
 
 export default function Home() {
   return (
-    <div className="mx-[15px]">
-      <div className="">
+    <div>
+      <div className="flex flex-col gap-4">
         <ProfileFrame />
-      </div>
 
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between">
-            <p className="text-sm">Aset Anda</p>
+        <div className="flex flex-col gap-6">
+          <div className="flex gap-2 overflow-x-auto scrollbar-hide ml-[15px]">
+            <TotalPortfolio />
 
-            <button className="text-xs font-semibold text-Primary-1">
-              Lihat Semua
-            </button>
+            <Balance />
           </div>
 
-          <div className="flex gap-4">
-            <Assets />
-          </div>
-        </div>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2 ml-[15px]">
+              <div className="flex justify-between">
+                <p className="text-sm">Aset Anda</p>
 
-        <div className="flex flex-col gap-2">
-          <p className="text-sm">Watchlist</p>
-          <div className="flex flex-col gap-2">
-            <Watchlist />
+                <button className="text-xs font-semibold text-Primary-1 mr-[15px]">
+                  Lihat Semua
+                </button>
+              </div>
+
+              <div className="flex gap-4 scrollbar-hide overflow-x-auto">
+                <Assets />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2 mx-[15px]">
+            <p className="text-sm">Watchlist</p>
+
+            <div className="flex flex-col gap-2">
+              <Watchlist />
+            </div>
           </div>
         </div>
       </div>
