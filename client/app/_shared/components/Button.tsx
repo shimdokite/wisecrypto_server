35acc483eb45c7ff404060a24f9ce8e8742bf34f
shimdokite <shimdokite@gmail.com>
2024-04-25 00:45:18 +0900

    [FE] ✨ Button 컴포넌트에 type 속성 추가

--- client/app/_shared/components/Button.tsx
+++ client/app/_shared/components/Button.tsx
@@ -1,10 +1,16 @@
 interface ButtonProps {
   children: string;
   color: 'green' | 'red';
+  type?: 'submit' | 'button';
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ children, color, onClick }: ButtonProps) {
+export default function Button({
+  children,
+  color,
+  type,
+  onClick,
+}: ButtonProps) {
   return (
     <button
       className={`${
@@ -12,6 +18,7 @@ export default function Button({ children, color, onClick }: ButtonProps) {
           ? 'bg-Primary-1 border-Primary-1'
           : 'bg-Danger-1 border-Danger-1'
       } w-full py-[11px] rounded-[4px] text-White-1 text-xs`}
+      type={type}
       onClick={onClick}>
       {children}
     </button>
