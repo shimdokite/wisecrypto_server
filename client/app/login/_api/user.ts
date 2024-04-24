3f4a1fecbf013a684f0625a9ee27933ea88b08ac
shimdokite <shimdokite@gmail.com>
2024-04-24 23:39:44 +0900

    [FE] 🐛 Login 타입 이름 변경(Login 페이지와 겹치는 이슈)

--- client/app/login/_api/user.ts
+++ client/app/login/_api/user.ts
@@ -2,9 +2,9 @@ import { setCookie } from 'cookies-next';
 
 import instance from '../../_shared/api/instance';
 
-import { Login } from '../_types/data';
+import { LoginInfo } from '../_types/data';
 
-export const postUserCredentials = async (login: Login) => {
+export const postUserCredentials = async (login: LoginInfo) => {
   const response = await instance.post('/login', login);
 
   const userId = response.data.id;
