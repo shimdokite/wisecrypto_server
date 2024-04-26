8e7ecbf125fa7745ea58013f7e0fda760cb21423
shimdokite <shimdokite@gmail.com>
2024-04-26 21:18:13 +0900

    [FE] ✨ Search 컴포넌트 구현

---
+++ client/app/main/_components/Search.tsx
@@ -0,0 +1,11 @@
+export default function Search() {
+  return (
+    <div className="mx-[15px]">
+      <input
+        type="text"
+        placeholder="Cari. . ."
+        className="w-full outline-none focus:shadow-inputShadow border-AquaSpring-1 border-[1px] rounded-lg pl-4 py-3 bg-[url('/assets/icon/green_search.svg')] bg-[center_right_16px] bg-no-repeat"
+      />
+    </div>
+  );
+}
