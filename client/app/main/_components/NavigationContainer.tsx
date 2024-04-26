a1a04f137c9a49d45c6ef94dd20eb3add41ee767
shimdokite <shimdokite@gmail.com>
2024-04-26 21:15:19 +0900

    [FE] ✨ 전역 상태에 따른 동적 렌더링 기능 구현

--- client/app/main/_components/NavigationContainer.tsx
+++ client/app/main/_components/NavigationContainer.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useState } from 'react';
+import useNavigationStore from 'store/navigationStore';
 
 import MenuPresentation from './MenuPresentation';
 
 export default function NavigationContainer() {
-  const [menu, setMenu] = useState('');
+  const { menu, setMenu } = useNavigationStore();
 
   return (
     <nav className="fixed left-0 right-0 bottom-0 w-full bg-White-1 flex items-center justify-evenly gap-[52.5px] px-8">
