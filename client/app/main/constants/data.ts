2626a03d7a871c6e0438edaf965d044f8c3f7275
shimdokite <shimdokite@gmail.com>
2024-05-01 01:55:23 +0900

    [FE] 📝 PROFILE_SETTING 데이터 추가

---
+++ client/app/main/constants/data.ts
@@ -0,0 +1,31 @@
+export const PROFILE_SETTING = {
+  transction_list: {
+    title: 'Daftar Transaksi',
+    description: 'Transakasi Yang Anda Miliki',
+    image: '/assets/icon/transaction_list.svg',
+  },
+
+  private: {
+    title: 'Privasi',
+    description: 'Ubah email dan password',
+    image: '/assets/icon/private.svg',
+  },
+
+  payment: {
+    title: 'Pembayaran',
+    description: 'Perbarui pengaturan pembayaran',
+    image: '/assets/icon/payment.svg',
+  },
+
+  notifications: {
+    title: 'Notifikasi',
+    description: 'Change notification settings',
+    image: '/assets/icon/notifications.svg',
+  },
+
+  resign: {
+    title: 'Keluar',
+    description: 'Keluar dari aplikasi',
+    image: '/assets/icon/resign.svg',
+  },
+};
