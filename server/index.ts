09b9f454a81337a721d2ba4bc7f4d6432b7336e0
shimdokite <shimdokite@gmail.com>
2024-05-02 16:41:26 +0900

    [BE] ✨ mysql 연동 테스트

--- server/index.ts
+++ server/index.ts
@@ -6,10 +6,26 @@ import loginRouter from './routes/users/loginRouter';
 import registerRouter from './routes/users/registerRouter';
 
 const dotenv = require('dotenv').config();
+const { SESSION_SECRET, DB_HOST, DB_USER, DB_PASS, CLIENT_ORIGIN } =
+	process.env;
 const app = express();
 const port = 3001;
+const connection = mysql.createConnection({
+	host: DB_HOST,
+	user: DB_USER,
+	password: DB_PASS,
+	database: 'wisecrypto',
+});
 
-const { SESSION_SECRET } = process.env;
+connection.connect();
+
+connection.query('SELECT * FROM User', (error, rows, fields) => {
+	if (error) throw error;
+
+	console.log(rows);
+});
+
+connection.end();
 
 app.get('/', (request: Request, response: Response) => {
 	response.send('Hello World');
