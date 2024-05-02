2568d65edaf082bd8675597752dbf617942a3be2
shimdokite <shimdokite@gmail.com>
2024-05-02 16:35:01 +0900

    [BE] ✨ express와 mysql 연동하기

--- server/controllers/users/registerController.ts
+++ server/controllers/users/registerController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
 
-import { db } from '../..';
-
 export const createAccount = (request: Request, response: Response) => {
 	const { name, phoneNumber, position, email, password } = request.body;
 
@@ -13,21 +11,21 @@ export const createAccount = (request: Request, response: Response) => {
 		password,
 	};
 
-	if (position === 'Pengguna') {
-		db.collection('users')
-			.doc('Pengguna')
-			.set({ ...userInfo });
+	// if (position === 'Pengguna') {
+	// 	db.collection('users')
+	// 		.doc('Pengguna')
+	// 		.set({ ...userInfo });
 
-		return response.status(201).send('Create Account!');
-	}
+	// 	return response.status(201).send('Create Account!');
+	// }
 
-	if (position === 'Pengelola') {
-		db.collection('users')
-			.doc('Pengelola')
-			.set({ ...userInfo });
+	// if (position === 'Pengelola') {
+	// 	db.collection('users')
+	// 		.doc('Pengelola')
+	// 		.set({ ...userInfo });
 
-		return response.status(201).send('Create Account!');
-	}
+	// 	return response.status(201).send('Create Account!');
+	// }
 
 	return response.status(400).send('Bad Request');
 };
--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,12 @@
 import express, { Request, Response } from 'express';
 import session from 'express-session';
 import cors from 'cors';
-
-import { cert, initializeApp, ServiceAccount } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccout from '../server/wisecrypto-50038-firebase-adminsdk-wclqn-073ed91895.json';
-
+import mysql from 'mysql';
 import loginRouter from './routes/users/loginRouter';
-
 import registerRouter from './routes/users/registerRouter';
 
-//! firebase 설정이 express 서버 보다 먼저 와야한다.
-initializeApp({ credential: cert(serviceAccout as ServiceAccount) });
-
-export const db = getFirestore();
-
 const dotenv = require('dotenv').config();
-
 const app = express();
-
 const port = 3001;
 
 const { SESSION_SECRET } = process.env;
@@ -26,7 +14,6 @@ const { SESSION_SECRET } = process.env;
 app.get('/', (request: Request, response: Response) => {
 	response.send('Hello World');
 });
-
 app.listen(port, () => {
 	console.log(`Server Port is ${port}`);
 });
