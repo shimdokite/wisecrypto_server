5aa026db1360270edd276095a149b53dd5a539e5
shimdokite <shimdokite@gmail.com>
2024-05-08 00:14:22 +0900

    [BE] ➕ cookie-parser 설치

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import loginRouter from './routes/users/loginRouter';
 import registerRouter from './routes/users/registerRouter';
+import userDetailRouter from './routes/users/userDetailRouter';
 
 const dotenv = require('dotenv').config();
 const app = express();
 const port = 3001;
+const corsOptions = {
+	origin: process.env.CLIENT_ORIGIN,
+	credentials: true,
+	methods: ['GET', 'POST', 'OPTIONS'],
+};
 
 app.get('/', (request: Request, response: Response) => {
 	response.send('Hello World');
@@ -15,15 +22,11 @@ app.listen(port, () => {
 	console.log(`Server Port is ${port}`);
 });
 
-const corsOptions = {
-	origin: process.env.CLIENT_ORIGIN,
-	credentials: true,
-	methods: ['GET', 'POST', 'OPTIONS'],
-};
-
+app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json({ strict: false }));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/accounts/:id', userDetailRouter);
