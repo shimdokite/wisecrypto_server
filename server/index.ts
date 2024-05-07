af7d690cc8a16beec65c11183b777829b3f8f5ef
shimdokite <shimdokite@gmail.com>
2024-05-07 13:36:58 +0900

    [BE] ♻️ db 파일 분리

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,12 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import mysql from 'mysql';
+
 import loginRouter from './routes/users/loginRouter';
 import registerRouter from './routes/users/registerRouter';
 
 const dotenv = require('dotenv').config();
 const app = express();
 const port = 3001;
-const { DB_HOST, DB_USER, DB_PASS, CLIENT_ORIGIN } = process.env;
-
-export const connection = mysql.createConnection({
-	host: DB_HOST,
-	user: DB_USER,
-	password: DB_PASS,
-	database: 'wisecrypto',
-});
-
-connection.connect((error) => {
-	if (error) return console.log(error.stack);
-
-	console.log('Successfully connecting!');
-});
 
 app.get('/', (request: Request, response: Response) => {
 	response.send('Hello World');
@@ -30,7 +16,7 @@ app.listen(port, () => {
 });
 
 const corsOptions = {
-	origin: CLIENT_ORIGIN,
+	origin: process.env.CLIENT_ORIGIN,
 	credentials: true,
 	methods: ['GET', 'POST', 'OPTIONS'],
 };
