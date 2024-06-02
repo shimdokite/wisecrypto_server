e70548d94c89e8caf6b996c615af4555ca8d4849
shimdokite <shimdokite@gmail.com>
2024-06-02 22:50:48 +0900

    [BE] 🐛 서버 연결 테스트

--- server/controllers/user/loginController.ts
+++ server/controllers/user/loginController.ts
@@ -40,11 +40,11 @@ export const matchUserInfomation = (request: Request, response: Response) => {
 
 			response.cookie('accessToken', `Bearer ${accessToken}`, {
 				httpOnly: true,
-				domain: '.wisecrypto-server.shop',
+				domain: 'api.wisecrypto-server.shop',
 			});
 			response.cookie('refreshToken', refreshToken, {
 				httpOnly: true,
-				domain: '.wisecrypto-server.shop',
+				domain: 'api.wisecrypto-server.shop',
 			});
 
 			return response.status(201).send('User logged in successfully.');
